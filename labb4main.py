90e386c3a9eda72c05f5fd19863920d40c882236
Ali Harith Rafik <aliharith.lazim@gmail.com>
2025-10-27 19:11:10 +0100

    labb 4(dess byg fixad) och 5

--- labb4main.py
+++ labb4main.py
@@ -1,25 +1,10 @@
 from bintreeFile import Bintree
 from linkedQFile import LinkedQ 
 
-class ParentNode:
-    def __init__(self, word, parent=None):
-        self.word = word
-        self.parent = parent
-        
-#Förklara denn del mer, finns i canvas, den används dock inte i koden direkt
-class SolutionFound (Exception):
-    pass
 
 
-def writechain(node): #Skriver ut ledjan från startord till slutord rekursivt
-    if node.parent is not None:
-        writechain(node.parent) #Gå till föräldern först
-    print(node.word) #skriv ut ordet 
-
-
-def makechildren(nod, slutord, svenska, gamla, q):
+def makechildren(ord, slutord, svenska, gamla, q):
     """ skapar alla barn till ett ord genom att byta ut en bokstav i taget"""
-    ord = nod.word
 
     alfabet = "abcdefghijlkmopqrstuvxyzåäö"
    
@@ -32,19 +17,16 @@ def makechildren(nod, slutord, svenska, gamla, q):
                 if nytt_ord in svenska and nytt_ord not in gamla:
                     gamla.put(nytt_ord)
 
-                    #Skapa ParentNode
-                    barn_nod = ParentNode(nytt_ord, nod)
-
-                    if nytt_ord == slutord:
-                        writechain(barn_nod)
-                        return True #Retunera True när hittad
-                    
-                    q.enqueue(barn_nod)
-
-    return False #Retunera False om inte hittad
+                if nytt_ord == slutord:
+                    print("Det finns en väg till", slutord)
+                    return True #Hittade slutordet!
+                q.enqueue(nytt_ord) #Lägg i kön                    
+    return False #Hittade inte slutordet 
 
 
-     
+
+
+
 
 
 
@@ -77,35 +59,35 @@ def main():
     gamla.put(startord) #startordet är redan "besökt"
 
 
+
+
     #För att hitta snabbaste vägen
 
     #Skapa kön och lägg in startordet
     q = LinkedQ()
-    startnod = ParentNode(startord) #skapa ParentNode för startord
-    q.enqueue(startnod)
-
-    hittad = False
+    q.enqueue(startord)
 
     #Huvudlisngan som skapar den snabbaste sökvägen
+    hittad = False
     while not q.isEmpty():
-        nod = q.dequeue()
+        word = q.dequeue() #Tar det första ordet från kön
 
-        
+       
 
-        if makechildren(nod,slutord,svenska,gamla, q):
+        if makechildren(word,slutord,svenska,gamla, q):
             hittad = True 
             break
 
     if not hittad:
             print("Det fanns ingen väg till ", slutord)
-
-
+   
 
 
 
 main()
 
          
+
 
 
 
