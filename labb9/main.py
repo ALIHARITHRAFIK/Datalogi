7ad6ec978ce0ce5b3fd85cb2c4f05714d27e0795
Ali Harith Rafik <aliharith.lazim@gmail.com>
2025-12-03 01:01:20 +0100

    labb 9 är helt klart

--- labb9/main.py
+++ labb9/main.py
@@ -8,7 +8,7 @@ godkanda_atomer = """H   He  Li  Be  B   C   N   O   F   Ne  Na  Mg  Al  Si  P  
 Mn  Fe  Co  Ni  Cu  Zn  Ga  Ge  As  Se  Br  Kr  Rb  Sr  Y   Zr  Nb  Mo  Tc  Ru  Rh  Pd  Ag  Cd
 In  Sn  Sb  Te  I   Xe  Cs  Ba  La  Ce  Pr  Nd  Pm  Sm  Eu  Gd  Tb  Dy  Ho  Er  Tm  Yb  Lu  Hf
 Ta  W   Re  Os  Ir  Pt  Au  Hg  Tl  Pb  Bi  Po  At  Rn  Fr  Ra  Ac  Th  Pa  U   Np  Pu  Am  Cm
-Bk  Cf  Es  Fm  Md  No  Lr  Rf  Db  Sg  Bh  Hs  Mt  Ds  Rg  Cn  Fl  Lv """
+Bk  Cf  Es  Fm  Md  No  Lr  Rf  Db  Sg  Bh  Hs  Mt  Ds  Rg  Cn  Fl  Lv """.split()
 
 class Syntaxfel(Exception):
     pass
@@ -16,6 +16,7 @@ class Syntaxfel(Exception):
 def formel(q):
     """<formel>::= <mol> \n"""
     mol(q)
+
     #Efter mol() ska kön vara tom
     if not q.isEmpty():
         raise Syntaxfel("Felaktig gruppstart vid radslutet " + resten_av_kon(q))
@@ -40,18 +41,23 @@ def group(q):
 
         #Nu måste det komma en )
         if q.isEmpty() or q.peek() != ')':
-            raise Syntaxfel("Saknad högerparentes vid radslutet" + resten_av_kon(q))
+            if q.isEmpty():
+                raise Syntaxfel("Saknad högerparentes vid radslutet")
+            else:
+                raise Syntaxfel("Saknad högerparentes vid radslutet " + resten_av_kon(q))
+    
+
     
         q.dequeue() #Ta bort ')'
 
         #Efter ) måste det komma ett tal
         if q.isEmpty() or not q.peek().isdigit():
-            raise Syntaxfel("Saknad siffra vid radslutet" + resten_av_kon(q))
+            raise Syntaxfel("Saknad siffra vid radslutet " + resten_av_kon(q))
     
         num(q)
 
     #Om gruppen börjar med stor bokstav
-    elif q.peek().isupper():
+    elif q.peek().isalpha(): #isalpha ser till att det är en bokstav, vare sig stor eller liten- 
         atom(q)
 
         #Om det följs av ett tal 
@@ -67,12 +73,6 @@ def group(q):
 
 
 
-def molekyl(q):
-    """   <molekyl> ::= <atom> | <atom><num>"""
-    atom(q) #Varje molekyl börjar med en atom - kontollera och ta bort den
-    if not q.isEmpty() and q.peek().isdigit():
-        num(q)
-
 
 
 def atom(q):
@@ -83,28 +83,25 @@ def atom(q):
     if not q.peek().isupper():
         raise Syntaxfel("Saknad stor bokstav vid radslutet " + resten_av_kon(q))
     
-    
-    #Ta upp stor bokstav
-    q.dequeue() 
+    #Läs atomnamnet
+    atom_namn = q.peek()
+    q.dequeue()
 
-    #Om nästa tecken är liten bokstav, ta upp den också
+    #Om nästa tecken är liten bokstav, lägg till den. Sker bara en bokstav åt gången
     if not q.isEmpty() and q.peek().islower():
-        letter(q)
+        atom_namn += q.peek()
+        q.dequeue()
+
+
+    #Kontrollera att atomen finns i listan
+    if atom_namn not in godkanda_atomer:
+        raise Syntaxfel("Okänd atom vid radslutet " + resten_av_kon(q))
+    
+    
 
 
  
 
-def LETTER(q):
-    """ <LETTER>::= A | B | C | ... | Z"""
-    if q.isEmpty() or not q.peek().isupper():
-        raise Syntaxfel("Saknad stor bokstav vid radslutet" +  resten_av_kon(q) )
-
-
-def letter(q):
-    """  <letter>::= a | b | c | ... | z"""
-    if q.isEmpty() or not q.peek().islower():
-        raise Syntaxfel("Saknad liten bokstav vid radslutet" +  resten_av_kon(q) )
-    q.dequeue()
      
 
 def num(q):
@@ -125,7 +122,7 @@ def num(q):
         q.dequeue() #Ta bort 1
         #Om inga fler siffror följer, då var det bara "1" vilket är fel
         if q.isEmpty() or not q.peek().isdigit():
-            raise Syntaxfel("För litet tal vid radslutet ")
+            raise Syntaxfel("För litet tal vid radslutet "+ resten_av_kon(q))
         #Annars fortsätt läsa resten av siffrorna (t.ex. "12" är OK)
     else:
         #Första siffran är 2-9, ta bort den 
@@ -154,7 +151,7 @@ def kolla_molekyl(molekyl_str):
     for loop in molekyl_str: #molekyl_str byts ut mot  inputen av användaren
         q.enqueue(loop) #Lägger varje tecken i kön
     try:
-        molekyl(q)
+        formel(q)
         return "Formeln är syntaktiskt korrekt"
     except Syntaxfel as e:
         return str(e)
@@ -164,14 +161,13 @@ def main():
     """Huvudprogrammet"""
     while True:
         line = input().strip()
-        if line == "#":
-            break
-        print(kolla_molekyl(line))
+        molekyler = line.split()
 
-
-
-if __name__ == '__main__':
-    main() #Kattis ignorerar allt efter detta, så Unittest är inte med
+        for mol in molekyler:
+            if mol == "#":
+                return #Avsluta helt
+            resultat = kolla_molekyl(mol)
+            print(resultat)
 
 
 
@@ -199,6 +195,12 @@ class TestMolekyl(unittest.TestCase):
         resultat = kolla_molekyl('cr12')
         self.assertIn('Saknad stor bokstav', resultat)
 
+        resultat = kolla_molekyl('Nacl')
+        self.assertIn('Saknad stor bokstav', resultat)
+
+
+
+
     def test_for_litet_tal(self):
         """Testa molekyler med 0 eller 1 som första siffra"""
         resultat = kolla_molekyl('Cr0')
@@ -210,6 +212,15 @@ class TestMolekyl(unittest.TestCase):
         resultat = kolla_molekyl('H01011')
         self.assertIn('För litet tal', resultat)
 
+        resultat = kolla_molekyl('H0')
+        self.assertIn('För litet tal', resultat)
+
+        resultat = kolla_molekyl('H1C')
+        self.assertIn('För litet tal', resultat)
+
+        resultat = kolla_molekyl('H02C')
+        self.assertIn('För litet tal', resultat)
+
     def test_syntaxfel_kastas(self):
         """Testa att Syntaxfel kastas korrekt"""
         #Skaoa en kö med ett fel i (liten bokstav)
@@ -219,6 +230,52 @@ class TestMolekyl(unittest.TestCase):
         #assertRaises förväntar sig att ett syntaxfel ska kastas
         with self.assertRaises(Syntaxfel):
             atom(q) #Anropar atom funktionen, den ska kasta ett syntaxfel
+
+#==================================== NYA TESTER FÖR LABB 9 ============================================
+    def test_sample_input_1(self):
+        self.assertEqual(kolla_molekyl('Na'), 'Formeln är syntaktiskt korrekt')
+        self.assertEqual(kolla_molekyl('H2O'), 'Formeln är syntaktiskt korrekt')
+        self.assertEqual(kolla_molekyl('Si(C3(COOH)2)4(H2O)7'), 'Formeln är syntaktiskt korrekt')
+        self.assertEqual(kolla_molekyl('Na332'), 'Formeln är syntaktiskt korrekt')
+
+    def test_okand_atom(self):
+        """Testa okänd atom"""
+        resultat = kolla_molekyl('C(Xx4)5')
+        self.assertIn('Okänd atom vid radslutet', resultat)
+
+    def test_saknad_siffra_efter_parantes(self):
+        """Testa sakand siffra efter parentes"""
+        resultat = kolla_molekyl('C(OH4)C')
+        self.assertIn('Saknad siffra vid radslutet', resultat)
+
+    def test_saknad_hogerparantes(self):
+        """Testa sakand saknad högerparentes"""
+        resultat = kolla_molekyl('C(OH4C')
+        self.assertIn('Saknad högerparentes vid radslutet', resultat)
+
+    def test_felaktig_gruppstart(self):
+        """Testa felaktig gruppstart"""
+        resultat = kolla_molekyl('H2O)Fe')
+        self.assertIn('Felaktig gruppstart vid radslutet', resultat)
+
+        resultat = kolla_molekyl('(Cl)2)3')
+        self.assertIn('Felaktig gruppstart vid radslutet', resultat)
+
+        resultat = kolla_molekyl(')')
+        self.assertIn('Felaktig gruppstart vid radslutet', resultat)
+
+        resultat = kolla_molekyl('2')
+        self.assertIn('Felaktig gruppstart vid radslutet', resultat)
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main() #Kattis ignorerar allt efter detta, så Unittest är inte med
+
+
+
 
 
 
